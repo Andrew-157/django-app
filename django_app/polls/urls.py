e4c1498e6455f8d6dc20df71ab94888ad38333db
Andrew Subotin <subotinandrey5@gmail.com>
2022-12-17 15:49:56 +0200

    basic paths

---
+++ django_app/polls/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('poll/<int:poll_id>', views.poll, name='poll')
+]
--- django_app/polls/views.py
+++ django_app/polls/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import Question
+
+
+def index(request):
+
+    template = loader.get_template('polls/index.html')
+
+    question_list = Question.objects.order_by('-published_at')[:10]
+
+    return HttpResponse(template.render({'question_list': question_list}, request))
+
+
+def poll(request, poll_id):
+
+    return HttpResponse(f'Poll: {poll_id}')
